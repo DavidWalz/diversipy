88675a07f694d079514cfa2be990b24f9d53a030
WalzDS <david-simon.walz@basf.com>
2020-02-19 09:03:53 +0100

    initial commit

---
+++ diversipy/__init__.py
@@ -0,0 +1,14 @@
+"""
+diversipy is a collection of algorithms for sampling in hypercubes,
+selecting diverse subsets, and measuring diversity.
+
+For further documentation see the submodules.
+"""
+
+from diversipy.hycusampling import *
+from diversipy.subset import *
+from diversipy.indicator import *
+from diversipy.distance import *
+
+
+__version__ = "0.8"
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="diversipy",
+    version="0.8",
+    author="Simon Wessing",
+    description="Sample in hypercubes, select diverse subsets, and measure diversity",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DavidWalz/diversipy",
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=["numpy", "scipy"],
+)
