130fb2968c62febac77e5739480fafe052620d27
WalzDS <david-simon.walz@basf.com>
2020-02-20 10:49:15 +0100

    travis

---
+++ test/test_indicator.py
@@ -0,0 +1,58 @@
+import numpy as np
+import diversipy
+
+
+def test_solow_polasky_diversity():
+    pass
+
+
+def test_weitzman_diversity():
+    pass
+
+
+def test_sum_of_dists():
+    pass
+
+
+def test_average_inverse_dist():
+    pass
+
+
+def test_separation_dist():
+    pass
+
+
+def test_binom_coeff():
+    pass
+
+
+def test_wmh_index():
+    pass
+
+
+def test_sum_of_nn_dists():
+    pass
+
+
+def test_unanchored_L2_discrepancy():
+    pass
+
+
+def test_expected_unanchored_L2_discrepancy():
+    pass
+
+
+def test_mean_dist_to_boundary():
+    pass
+
+
+def test_expected_dist_to_boundary():
+    pass
+
+
+def test_averaged_hausdorff_dist():
+    pass
+
+
+def test_hausdorff_dist():
+    pass
